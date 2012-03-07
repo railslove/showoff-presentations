bc348699e34fcfa472d9ef0825205e8fb9514bfc
Salim Hbeiliny <salim@railslove.com>
2012-03-07 23:32:05 +0100

    Added print friendly display
    Best use in Safari
    Press Alt + P, then print the page (save as PDF)

--- template/railslove.js
+++ template/railslove.js
@@ -7,6 +7,25 @@
 })();
 
 (function( $ ) {
+
+  $(document).keypress(function(e) {
+    if(e.altKey && e.which == 960){
+      //detect Alt + P keypress
+      $('.slide').css({
+        display: 'block',
+        opacity: 1,
+        position: 'relative'
+      });
+      $('body').css('overflow', 'auto');
+      $('#preso').css('overflow', 'visible');
+      $('.slide').after('<div class="page-break"></div>');
+
+      centerSlides($('.slide'));
+
+      alert("Now you can print using safari's normal print option\nDon't forget to check the print backgrounds option, and preferrably use landscape mode.");
+    }
+  });
+
   $(document).ready(function() {
 
     $(document).bind("showoff:loaded", function (event) {
